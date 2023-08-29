0dc3009f267276ab76108af95e550e8bc3df5c3d
Euphokumiko <h.k.h94539@gmail.com>
2023-08-29 23:59:25 +0800

    fixed carousel component auto playthrough

--- src/app/shared/components/pic-carousel/pic-carousel.component.ts
+++ src/app/shared/components/pic-carousel/pic-carousel.component.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
-import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
+import { AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
 
 import { environment } from 'src/environments/environment';
 
@@ -10,12 +10,14 @@ import { environment } from 'src/environments/environment';
 })
 export class PicCarouselComponent implements OnInit, OnChanges {
   @ViewChild('picCarousel') picCarousel!: NgbCarousel;
-  @ViewChild('mov1') mov1!: HTMLVideoElement;
-  @ViewChild('mov2') mov2!: HTMLVideoElement;
-  @ViewChild('pic1') pic1!: HTMLImageElement;
-  @ViewChild('pic2') pic2!: HTMLImageElement;
+  @ViewChild('mov1') mov1!: ElementRef<HTMLVideoElement>;
+  @ViewChild('mov2') mov2!: ElementRef<HTMLVideoElement>;
+  @ViewChild('pic1') pic1!: ElementRef<HTMLImageElement>;
+  @ViewChild('pic2') pic2!: ElementRef<HTMLImageElement>;
 
   assetUrl: string;
+
+  timeOutToClear: NodeJS.Timeout | null = null;
 
   @Input()
   enzaId!: number;
@@ -34,14 +36,69 @@ export class PicCarouselComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    //console.log(this.mov1, this.mov2, this.pic1, this.pic2)
+    if (!this.picCarousel) { return; }
+    this.picCarousel.select("img1");
+    this.playThrough();
+  }
+
+  ngAfterViewInit(): void {
+    if (this.timeOutToClear) {
+      clearTimeout(this.timeOutToClear);
+      this.timeOutToClear = null;
+    }
+    this.playThrough();
+  }
+
+  playThrough(): void {
+    console.log(this.picCarousel.activeId);
+    if (this.picCarousel.activeId === "mov1") {
+      this.mov1.nativeElement.play();
+    }
+    else if (this.picCarousel.activeId === "mov2") {
+      this.mov2.nativeElement.play();
+    }
+    else {
+      if (this.timeOutToClear) {
+        clearTimeout(this.timeOutToClear);
+        this.timeOutToClear = null;
+      }
+      this.timeOutToClear = setTimeout(() => {
+        this.picCarousel.next();
+        this.playThrough();
+      }, 5000);
+    }
+  }
+
+  onSlide(slideEvent: NgbSlideEvent) {
+    if (!slideEvent.source) { return; }
+    switch (this.picCarousel.activeId) {
+      case "mov1":
+        this.mov1.nativeElement.pause();
+        this.mov1.nativeElement.currentTime = 0;
+        break;
+      case "mov2":
+        this.mov2.nativeElement.pause();
+        this.mov2.nativeElement.currentTime = 0;
+        break;
+      default:
+        break;
+    }
+  }
+
+  onSlid(slideEvent: NgbSlideEvent) {
+    if (!slideEvent.source) { return; }
+    if (this.timeOutToClear) {
+      clearTimeout(this.timeOutToClear);
+      this.timeOutToClear = null;
+    }
+    this.playThrough();
   }
 
   ngOnInit(): void {
-
   }
 
   onMovieEnded(): void {
-
+    this.picCarousel.next();
+    this.playThrough();
   }
 }
