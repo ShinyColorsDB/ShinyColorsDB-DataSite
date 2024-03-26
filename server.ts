aaedbbfff3174f8d3dfa5fcfaf6d186972518c40
Euphokumiko <h.k.h94539@gmail.com>
2024-03-26 13:40:55 +0800

    downgrade to pixi.js v7 due to serverless function crash

--- server.ts
+++ server.ts
@@ -35,6 +35,7 @@ export function app(): express.Express {
         url: `${protocol}://${headers.host}${originalUrl}`,
         publicPath: browserDistFolder,
         providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+        inlineCriticalCss: false,
       })
       .then((html) => res.send(html))
       .catch((err) => next(err));
