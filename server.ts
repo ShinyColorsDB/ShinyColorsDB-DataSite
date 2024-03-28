e04f07acbd7d6da05f2ab3b5cb2e7500d577cc91
Euphokumiko <h.k.h94539@gmail.com>
2024-03-28 11:37:33 +0800

    dev i18n

--- server.ts
+++ server.ts
@@ -4,9 +4,11 @@ import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
+import { REQUEST, RESPONSE } from 'src/express.tokens';
+import { LOCALE_ID } from '@angular/core';
 
 // The Express app is exported so that it can be used by serverless Functions.
-export function app(): express.Express {
+export function app(lang: string): express.Express {
   const server = express();
   const serverDistFolder = dirname(fileURLToPath(import.meta.url));
   const browserDistFolder = resolve(serverDistFolder, '../browser');
@@ -34,7 +36,13 @@ export function app(): express.Express {
         documentFilePath: indexHtml,
         url: `${protocol}://${headers.host}${originalUrl}`,
         publicPath: browserDistFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+        providers: [
+          { provide: APP_BASE_HREF, useValue: baseUrl },
+          { provide: LOCALE_ID, useValue: lang },
+          { provide: RESPONSE, useValue: res },
+          { provide: REQUEST, useValue: req }
+        ],
+        inlineCriticalCss: false,
       })
       .then((html) => res.send(html))
       .catch((err) => next(err));
@@ -46,8 +54,13 @@ export function app(): express.Express {
 function run(): void {
   const port = process.env['PORT'] || 4000;
 
+  const appTW = app('zh-TW'),
+    appJP = app('ja-JP');
+
   // Start up the Node server
-  const server = app();
+  const server = express();
+  server.use('/', appTW);
+  server.use('/jp', appJP);
   server.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
